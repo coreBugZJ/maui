e17604e799c52bee5db81b8c3dd58e8081aecfd7
Shane Neuville <shneuvil@microsoft.com>
2022-10-21 18:41:10 -0500

    - Frame tests

--- src/Controls/tests/DeviceTests/Elements/Frame/FrameTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Frame/FrameTests.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Platform;
@@ -10,6 +12,30 @@ using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
 {
+	public class FrameStub : Frame, IStubBase
+	{
+		public double MaximumWidth { get; set; }
+		public double MaximumHeight { get; set; }
+		public double MinimumWidth { get; set; }
+		public double MinimumHeight { get; set; }
+		public Visibility Visibility { get; set; }
+		public Semantics Semantics { get; set; }
+		double IStubBase.Width { get; set; }
+		double IStubBase.Height { get; set; }
+		Paint IStubBase.Background { get; set; }
+		IShape IStubBase.Clip { get; set; }
+		IElement IStubBase.Parent { get; set; }
+	}
+
+	[Category(TestCategory.Frame)]
+	public class FrameHandlerTest : HandlerTestBase<FrameRenderer, FrameStub>
+	{
+		public FrameHandlerTest()
+		{
+
+		}
+	}
+
 	[Category(TestCategory.Frame)]
 	public partial class FrameTests : ControlsHandlerTestBase
 	{
